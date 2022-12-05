6a12a637d0f1054933e79035ff241801f72490d8
Sunny Golovine <sunny@glvn.co>
2022-12-05 10:27:03 -0700

    make mouse loop infinite

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mouse Mover
+
+This is a simple NodeJS app that will move your mouse on the screen. Once the app starts running you will no longer have control over the mouse. To exit the application press "CTRL + C"
+
+# LICENSE
+
+CC0 / Public Domain
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // Move the mouse across the screen as a sine wave.
-var robot = require("robotjs");
+const robot = require("robotjs");
 
-// Speed up the mouse.
-robot.setMouseDelay(2);
+(() => {
+  console.clear();
+  console.log("Mouse Mover is running. Press CTRL + C to exit.");
+  // Speed up the mouse.
+  robot.setMouseDelay(2);
 
-var twoPI = Math.PI * 2.0;
-var screenSize = robot.getScreenSize();
-var height = (screenSize.height / 2) - 10;
-var width = screenSize.width;
+  const twoPI = Math.PI * 2.0;
+  const screenSize = robot.getScreenSize();
+  const height = screenSize.height / 2 - 10;
+  const width = screenSize.width;
 
-for (var x = 0; x < width; x++)
-{
-	y = height * Math.sin((twoPI * x) / width) + height;
-	robot.moveMouse(x, y);
-}
+  let x;
+
+  while (true) {
+    if (x > width / 2) {
+      x = 0;
+    }
+    for (x = 0; x < width; x++) {
+      y = height * Math.sin((twoPI * x) / width) + height;
+      robot.moveMouse(x, y);
+    }
+  }
+})();
